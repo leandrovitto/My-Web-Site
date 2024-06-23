950393feb9576e92a3473d57ddc055829cf1ea64
Leandro Vitto <vitto.leandro@gmail.com>
2024-06-23 16:44:45 +0200

    Update tech skills in frontend, micro frontend, and test e2e areas. Added new skills and technologies like Next Js, Nuxt Js, Micro Frontend (Piral Js), Single SPA, Module Federation Webpack 5, Piral CLI, Playwright Js, Cucumber Js / Gerkin. Refine existing skills descriptions.

--- content/techskills/_it_techskills.js
+++ content/techskills/_it_techskills.js
@@ -3,12 +3,33 @@ const techSkills = [
         title: 'Frontend Skills',
         icon: 'react',
         skills: [
-            "React Js, Next Js, Redux, Redux Saga / Vue Js, Nuxt Js & Vuex",
-            "Node Js, Express Js, Nest Js / Micro Frontend (Piral Js)",
+            "React Js, Redux, Redux Saga / Vue Js, Vuex",
+            "Next Js & Nuxt Js",
+            "Node Js",
             "CSS / SCSS / Tailwind UI, Material UI, Bootstrap",
             "Common Lib / Webpack, Gulp Js, Rollback Js, Vite Js",
-            " Test integration / e2e: Jest Js, Cypress Js",
-            "TDD Programming, Integration Via Artifact, Docker / Docker Compose,Payment Gateway Stripe Js e Braintree JS",
+            "TDD Programming,Payment Gateway Stripe Js e Braintree JS",
+        ]
+    },
+    {
+        title: 'Micro Frontend',
+        icon: 'nextjs',
+        skills: [
+            "Architectures: Single SPA, Module Federation Webpack 5",
+            "Piral Js e Piral CLI",
+            "Integration Via Artifact",
+            "NPM Private Registry Nexus / Verdaccio",
+            "Lerna Js / Turbo Js",
+            "Monorepository"
+        ]
+    },
+    {
+        title: 'Test e2e',
+        icon: 'javascript',
+        skills: [
+            "Playwright Js / Cypress Js",
+            "Cucumber Js / Gerkin",
+            "Jest Js",
         ]
     },
     {
@@ -16,10 +37,10 @@ const techSkills = [
         icon: 'heroku',
         skills: [
             "Versioning Git, Bitbucket e Github",
+            "Docker / Docker Compose",
             "Agile Method Atlassian Jira, Scrum, Kanban",
             "Pipeline Bitbucket, Semaphore CI",
-            "Heroku e Digital Ocean",
-            "Google Cloud, Amazon AWS, Azure (Basic Structure)",
+            "Heroku, Digital Ocean,Google Cloud, Amazon AWS, Azure (Basic Structure)",
             "Kubernetes (Basic Structure)",
         ]
     },
@@ -27,15 +48,15 @@ const techSkills = [
         title: 'Backend Background',
         icon: 'nodejs',
         skills: [
+            "Node Js (Express Js) e Next Js",
             "Springbot (Java)",
             "Laravel 8 (PHP)",
-            "Node Js (Express Js)",
             "PostgreSQL, MySQL, Redis, Amazon Cache SQS",
         ]
     },
     {
         title: 'Programming languages',
-        icon: 'javascript',
+        icon: 'java',
         skills: [
             "Javascript",
             "JAVA",
