d6af104ee0a2fe6b78c2ac3ea125d9bf5ac656db
Leandro <vitto.leandro@gmail.com>
2025-04-13 13:32:51 +0200

    fix: Update YouTube subscriber count in about-typewriter content

--- src/components/layouts/AboutTypewriter.tsx
+++ src/components/layouts/AboutTypewriter.tsx
@@ -1,32 +1,34 @@
 import useTranslation from "next-translate/useTranslation";
 import Typewriter from "typewriter-effect";
-import { FunctionComponent, ReactElement } from 'react';
+import { FunctionComponent, ReactElement } from "react";
 
 const AboutTypewriter: FunctionComponent = (): ReactElement => {
-    const { t } = useTranslation("common");
+  const { t } = useTranslation("common");
 
-    const base = t("about-typewriter-base");
-    const tArray = t(
-        "about-typewriter-content",
-        { count: 1 },
-        { returnObjects: true }
-    ) as Array<string>;
+  const base = t("about-typewriter-base");
+  const tArray = t(
+    "about-typewriter-content",
+    {},
+    {
+      returnObjects: true,
+    }
+  ) as Array<string>;
 
-    const typewriterOptions = {
-        delay: 90, // speed up insertion
-        deleteSpeed: 40, // speed up deletion
-        strings: tArray,
-        autoStart: true,
-        loop: true,
-        cursor: "_",
-    };
+  const typewriterOptions = {
+    delay: 90, // speed up insertion
+    deleteSpeed: 40, // speed up deletion
+    strings: tArray,
+    autoStart: true,
+    loop: true,
+    cursor: "_",
+  };
 
-    return (
-        <>
-            {base}
-            <Typewriter options={typewriterOptions} />
-        </>
-    );
-}
+  return (
+    <>
+      {base}
+      <Typewriter options={typewriterOptions} />
+    </>
+  );
+};
 
 export default AboutTypewriter;
